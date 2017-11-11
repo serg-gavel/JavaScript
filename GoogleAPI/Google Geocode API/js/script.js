5ede332bc5e57070f73ca36313748552c884920b
Sergey Gavelyuk <gavelyuk.sergey.13@gmail.com>
2017-11-11 17:37:56 +0200

    update repository

--- GoogleAPI/Google Geocode API/js/script.js
+++ GoogleAPI/Google Geocode API/js/script.js
@@ -22,8 +22,33 @@ function geoCode() {
             </ul>
             `;
 
+            //Address Components
+            var addressComponents = response.data.results[0].address_components;
+            var addressComponentsOutput = '<ul class="list-group">';
+            for(var i = 0; i < addressComponents.length; i++){
+                addressComponentsOutput +=`
+                    <li class="list-group-item"><strong>
+                        ${addressComponents[i].types[0]}
+                    </strong>: ${addressComponents[i].long_name}</li>
+                `;
+            }
+            addressComponentsOutput +='</ul>';
+
+            // Geometry
+            var lat = response.data.results[0].geometry.location.lat;
+            var lng = response.data.results[0].geometry.location.lng;
+            var geometryOutput = `
+            <ul class="list-group">
+                <li class="list-group-item"><strong>Lattitude</strong>: ${lat}</li>
+                <li class="list-group-item"><strong>Longitude</strong>: ${lng}</li>
+            </ul>
+            `;
+
+
             // Output to app
             document.getElementById('formatted-address').innerHTML = formattedAddressOutput;
+            document.getElementById('address-components').innerHTML = addressComponentsOutput;
+            document.getElementById('geometry').innerHTML = geometryOutput;
         })
         .catch(function (error) {
             console.log(error);
