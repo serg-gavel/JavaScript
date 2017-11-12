c45f38aa5d93c9ade28eb2c401eece64245b672a
Sergey Gavelyuk <gavelyuk.sergey.13@gmail.com>
2017-11-12 23:00:44 +0200

    update repository

--- AJAX/Local_JSON/js/script.js
+++ AJAX/Local_JSON/js/script.js
@@ -1,4 +1,5 @@
 document.getElementById('button1').addEventListener('click', loadUser);
+document.getElementById('button2').addEventListener('click', loadUsers);
 
 function loadUser() {
     var xhr = new XMLHttpRequest();
@@ -8,7 +9,48 @@ function loadUser() {
         if(this.status === 200){
             var user = JSON.parse(this.responseText);
             // console.log(this.responseText);
-            console.log(user.name);
+            // console.log(user.name);
+            var output ='';
+
+            output += '<ul>'+
+                        '<li><strong>ID: </strong>'+user.id+'</li>'+
+                        '<li><strong>Name: </strong>'+user.name+'</li>'+
+                        '<li><strong>Email: </strong>'+user.email+'</li>'+
+                       '</ul>';
+            document.getElementById('user').innerHTML = output;
+        }
+        else if(this.status === 404){
+            console.log('404 (Not found)');
+        }
+    };
+    xhr.send();
+}
+
+function loadUsers() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'users.json', true);
+
+    xhr.onload = function () {
+        if(this.status === 200){
+            var users = JSON.parse(this.responseText);
+            // console.log(this.responseText);
+            // console.log(users.name);
+            var output ='';
+
+            //for in loop
+            for(var i in users){
+                // for in hasOwnProperty check
+                if (users.hasOwnProperty(i)) {
+                    output += '<ul>'+
+                                '<li><strong>ID: </strong>'+users[i].id+'</li>'+
+                                '<li><strong>Name: </strong>'+users[i].name+'</li>'+
+                                '<li><strong>Email: </strong>'+users[i].email+'</li>'+
+                              '</ul>';
+
+                }
+            }
+
+            document.getElementById('users').innerHTML = output;
         }
         else if(this.status === 404){
             console.log('404 (Not found)');
