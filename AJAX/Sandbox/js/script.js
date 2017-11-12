a94c15be5eb49eb923a128b92fdeb623df0e876b
Sergey Gavelyuk <gavelyuk.sergey.13@gmail.com>
2017-11-12 18:41:08 +0200

    update repository

--- AJAX/Sandbox/js/script.js
+++ AJAX/Sandbox/js/script.js
@@ -1,5 +1,19 @@
 document.getElementById('button').addEventListener('click', loadText);
 
 function loadText() {
-    console.log('button clicked');
+// Create XHR Object
+    var xhr = new XMLHttpRequest();
+    // OPEN - type, url/file, async(true or false)
+    xhr.open('GET', 'sample.txt', true);
+
+    xhr.onload = function () {
+        // 'this.status' the same like 'xhr.status'
+        if(this.status === 200){
+            console.log(this.responseText);
+        }
+    }
 }
+// HTTP Statuses :
+// 200: 'OK'
+// 403: 'Forbidden'
+// 200: 'Not Found'
