e833e8c4914d99c98e4c8829f71518d923a5bd96
Sergey Gavelyuk <gavelyuk.sergey.13@gmail.com>
2017-11-15 01:19:17 +0200

    update

---
+++ VueSandbox/vue-app/src/components/Test.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="test">
+    <h1>{{title}}</h1>
+    <p>{{user.firstName}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'test',
+    data(){
+      return{
+        title: 'Hello my Lord!',
+        user:{
+          firstName: 'Serg',
+          lastName: 'Gavel'
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
