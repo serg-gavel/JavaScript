80c3fc82220291d3e06f476e1c37e2675dd8cb5c
Sergey Gavelyuk <serg-gavel@users.noreply.github.com>
2017-11-15 22:24:12 +0200

    Add files via upload

--- VueSandbox/vue-app/src/main.js
+++ VueSandbox/vue-app/src/main.js
@@ -1,15 +1,41 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import App from './App'
-// import router from './router'
+import Users from './components/users.vue'
+import Test from './components/Test.vue'
 
-Vue.config.productionTip = false
+Vue.use(VueResource);
+Vue.use(VueRouter);
+
+Vue.config.productionTip = false;
+
+
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes:[
+    {path:'/', component: Users},
+    {path:'/test', component: Test}
+  ]
+});
+
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  // router,
-  template: '<App/>',
-  components: { App }
-})
+  // el: '#app',
+  router,
+  template:`
+    <div id="app">
+      <ul>
+        <li><router-link to="/">Users</router-link></li>
+        <li><router-link to="/test">Test</router-link></li>
+      </ul>
+      <router-view></router-view>
+    </div>
+`
+  // template: '<App/>',
+  // components: { App }
+}).$mount('#app');
