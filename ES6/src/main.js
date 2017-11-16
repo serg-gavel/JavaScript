2aab526d69db23c708519ee244d061132aeba673
Sergey Gavelyuk <gavelyuk.sergey.13@gmail.com>
2017-11-16 22:20:07 +0200

    update repository

--- ES6/src/main.js
+++ ES6/src/main.js
@@ -32,8 +32,8 @@ class Member extends User{
 }
 
 let mike = new Member('mike','mike@hotmail.com','1221214','"Standard"');
-mike.getPackage();
-mike.register();
+// mike.getPackage();
+// mike.register();
 
 // TEMPLATE
 let name = 'John';
@@ -48,3 +48,28 @@ let template = `
 `;
 
 document.getElementById('template').innerHTML = template;
+
+// New String & Number Methods
+
+let theString = 'Hello, my name is Sergey and I love JavaScript';
+
+// startWith();
+// endsWith();
+// includes();
+
+// console.log(theString.startsWith('Hello'));
+// console.log(theString.endsWith('JavaScript'));
+// console.log(theString.includes('name'));
+
+// // Hex - ES5
+// console.log(0xFF);
+// // Binary - ES6 support
+// console.log(0b101011);
+// // Octal
+// console.log(0o543);
+
+// console.log(Number.isFinite(NaN));
+// console.log(Number.isNaN('string'));
+// console.log(Number.isInteger(2));
+
+
