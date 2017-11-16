d11075336f0a420a13c1b5e34c3a154fbfdbc810
Sergey Gavelyuk <gavelyuk.sergey.13@gmail.com>
2017-11-16 19:43:36 +0200

    update repository

---
+++ ES6/src/main.js
@@ -0,0 +1,21 @@
+// function testVar() {
+//     var a = 20;
+//     if(true){
+//         var a = 30;
+//         console.log(a);
+//     }
+//     console.log(a);
+// }
+//
+// testVar();
+
+
+function testLet() {
+    let a = 20;
+    if(true){
+        let a = 30;
+        console.log(a);
+    }
+    console.log(a);
+}
+testLet();
