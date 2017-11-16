68cfc22d4c0e429c56e0de935c66743bb6fb17f7
Sergey Gavelyuk <gavelyuk.sergey.13@gmail.com>
2017-11-16 22:36:10 +0200

    update repository

--- ES6/src/main.js
+++ ES6/src/main.js
@@ -74,7 +74,28 @@ document.getElementById('template').innerHTML = template;
 
 //Default Params & Spread Operator
 
+// Default Params
 function greet($greeting = 'Hello World') {
     console.log($greeting);
 }
 greet();
+
+
+// ES5
+// var args = [1,2,3];
+//
+// function test(){
+//     console.log(args);
+// }
+//
+// test.apply(null, args);
+// Spread Operator
+let args1 = [1,2,3];
+let args2 =[4,5,6];
+function test(){
+    console.log(args1+','+args2);
+}
+
+test(...args1,...args2);
+
+//Set, Map, WeakSet and WeakMap
