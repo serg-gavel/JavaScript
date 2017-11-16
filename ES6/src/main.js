e6cfa2956f3747499660558e3f9fbd620aad3e70
Sergey Gavelyuk <gavelyuk.sergey.13@gmail.com>
2017-11-16 19:48:58 +0200

    update repository

--- ES6/src/main.js
+++ ES6/src/main.js
@@ -1,11 +1,14 @@
 class User{
     constructor(username, email, password){
-        thiis.username = username;
-        thiis.email = email;
-        thiis.password = password;
+        this.username = username;
+        this.email = email;
+        this.password = password;
     }
 
     register(){
         console.log(this.username+ ' is now registered');
     }
 }
+
+let bob = new User('bob', 'bob@gmail.com', '21412ssa1');
+bob.register();
