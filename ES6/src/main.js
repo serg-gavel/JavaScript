3c265793d7a8bb7c5288f26630e60eda2ddaf195
Sergey Gavelyuk <gavelyuk.sergey.13@gmail.com>
2017-11-16 20:00:01 +0200

    update repository

--- ES6/src/main.js
+++ ES6/src/main.js
@@ -5,10 +5,31 @@ class User{
         this.password = password;
     }
 
+    static countUsers(){
+        console.log('There are 50 users');
+    }
+
     register(){
         console.log(this.username+ ' is now registered');
     }
 }
 
-let bob = new User('bob', 'bob@gmail.com', '21412ssa1');
-bob.register();
+// let bob = new User('bob', 'bob@gmail.com', '21412ssa1');
+// bob.register();
+//
+// User.countUsers();
+
+class Member extends User{
+    constructor(username, email, password, memberPackage ){
+        super(username, email, password);
+        this.package = memberPackage;
+    }
+
+    getPackage(){
+        console.log(this.username+' is subscribed to the '+this.package+' package');
+    }
+}
+
+let mike = new Member('mike','mike@hotmail.com','1221214','"Standard"');
+mike.getPackage();
+mike.register();
